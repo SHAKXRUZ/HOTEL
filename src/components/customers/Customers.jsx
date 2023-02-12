2e90c7186c62060013655088e7c821334397d370
shaxruz25 <shaxruzismoilov250@gmail.com>
2023-02-12 19:34:50 +0500

    customers start

--- src/components/customers/Customers.jsx
+++ src/components/customers/Customers.jsx
@@ -1,5 +1,16 @@
 import "./Customers.css";
 const Customers = () => {
-  return <div>Customers</div>;
+  return (
+    <div className="customers">
+      <div className="customers_container">
+        <div className="customers_titles">
+          <h2 className="customers_titles_title">Our Happy Customers</h2>
+          <p className="customers_titles_text">
+            Know about our clients, we are a woldwide corporate brand
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Customers;
